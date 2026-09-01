3bce3fea052e16247561b031a56102bff4a53a65
Deepesh1024 <d4deepeshpro@gmail.com>
2026-09-01 16:00:30 +0530

    adding experiment details to docs

--- inspect_datasets.py
+++
@@ -1,8 +0,0 @@
-from datasets import get_dataset_config_names, load_dataset
-
-mmlu_configs = get_dataset_config_names("cais/mmlu")
-print("MMLU Configs (Categories):", len(mmlu_configs))
-print("Sample MMLU Configs:", mmlu_configs[:15])
-
-arc_configs = get_dataset_config_names("ai2_arc")
-print("ARC Configs:", arc_configs)
